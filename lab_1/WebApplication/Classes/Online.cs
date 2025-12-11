61328795af467537995e7fd2c1fb888ebc876650
chel1k_hohmas <popilev.sanya@gmail.com>
2025-12-12 00:38:43 +0300

    Update Online.cs

--- lab_1/WebApplication/Classes/Online.cs
+++ lab_1/WebApplication/Classes/Online.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 namespace WebApplication.Classes{
     public class Online : Course
     {
-        private string zoom { get; }
+        private string zoom { get; set;}
 
         public Online(string name, string link) : base(name)
         {
             zoom = link;
         }
     }
+
 }
