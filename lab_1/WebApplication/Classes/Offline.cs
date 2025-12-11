6be65744d0030b0ea3decd8280ea07283bf53759
chel1k_hohmas <popilev.sanya@gmail.com>
2025-12-12 00:38:58 +0300

    Update Offline.cs

--- lab_1/WebApplication/Classes/Offline.cs
+++ lab_1/WebApplication/Classes/Offline.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 namespace WebApplication.Classes{
     public class Offline : Course
     {
-        private string adress { get; }
-        private string auditore { get; }
+        private string adress { get; set; }
+        private string auditore { get; set; }
 
         public Offline(string name, string ad, string audi) : base(name)
         {
@@ -12,4 +12,5 @@ namespace WebApplication.Classes{
             auditore = audi;
         }
     }
+
 }
